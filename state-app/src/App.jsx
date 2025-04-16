99c13442af637035006f7ec6ad71ff82fcd14c89
Leo Rodriguez <leorod18@yahoo.com>
2025-04-16 09:44:10 -0400

    pt 1 done

--- state-app/src/App.jsx
+++ state-app/src/App.jsx
@@ -7,37 +7,68 @@ export default function App() {
   // const [] = useState(); state is a special var that can update screen
   // let step = 1;
   const [step, setStep] = useState(1);
+  const [show, setShow] = useState(true);
 
   // this is just javascript
   const prevHandler = () => {
-    alert("previous");
+    // setStep 1
+    // setStep(1);
+    // minus 1
+    // if statement to prevent negative
+    // check if step is something, >, >=, <, <=, =
+    if (step > 1) {
+      setStep(step - 1);
+    }
   }
+
   // create nextHandler and alert next
   // connect it to Next button
-  const nextHandler =
-    () => { 
-      setStep(2);
+  const nextHandler = () => {
+    // setStep();
+    // only when true
+    if (step < 3) {
+      setStep(step + 1);
     }
-
+  }
+  // CLOSE
+  if (show == false) {
+    return (
+      <div>
+        <button
+          onClick={
+            () => { setShow(true) }
+          }>X</button>
+      </div>
+    )
+  }
+  // OPEN
   return (
     <div>
       {/* Close */}
       <div>
-        <button>X</button>
+        {/* onClick={} */}
+        <button onClick={
+          () => {
+            setShow(false);
+          }
+        }>X</button>
       </div>
       {/* Circle Component */}
       <CircleComponent
+        stateStep={step}
         step={1}
       />
       <CircleComponent
+        stateStep={step}
         step={2}
       />
       <CircleComponent
+        stateStep={step}
         step={3}
       />
       {/* Text */}
       <h3>Learn React</h3>
-      <p>this is step {step}</p>
+      <p>this is state step {step}</p>
       {/* Buttons */}
       <button onClick={prevHandler}>Prev</button>
       <button onClick={nextHandler}>Next</button>
--- state-app/src/CircleComponent.jsx
+++ state-app/src/CircleComponent.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 // default means it the only thing being exported
 export default function CircleComponent(props) {
+
   return (
     <div style={{
       border: "2px solid black",
       padding: 5,
       display: "inline-block",
+      backgroundColor:
+        props.step <= props.stateStep ?
+          "pink" : "white",
       // dont worry about below
       height: 40,
       width: 40,
       borderRadius: "50%",
       justifyContent: "center",
-      alignItems:'center'
+      alignItems: 'center'
     }}>
       <p>{props.step}</p>
     </div>
