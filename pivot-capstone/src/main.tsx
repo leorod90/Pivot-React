1ea29e6924c5372c072151b58c12d40b35c0ec18
Leo Rodriguez <leorod18@yahoo.com>
2025-06-04 19:33:51 -0400

    pushed

--- pivot-capstone/src/main.tsx
+++ pivot-capstone/src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom';
 import Navigation from "./navigation";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
       <Navigation />
-    </BrowserRouter>
   </StrictMode>,
 )
